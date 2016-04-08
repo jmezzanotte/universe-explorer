34ff655be672323440ac87ffce96e3dcbcefb762
jmezzanotte <johnjmezzanotte@gmail.com>
2016-04-08 14:46:16 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php 
 
+  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    }
   include_once("planets.html"); 
-  header("Access-Control-Allow-Origin: *");
+ 
 
 ?>
