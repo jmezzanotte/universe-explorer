7f273ee17a4432e7143593de4ab6d1840b6ef2b3
jmezzanotte <johnjmezzanotte@gmail.com>
2016-05-01 22:15:54 -0700

    Update connect.php

--- resources/connect.php
+++ resources/connect.php
@@ -1,7 +1,7 @@
 <?php 
 	// enhance security by parsing the url to get the database information instead 
 	// this way you don't have to reveal it here in the code
-	$dbc = mysql_connect('us-cdbr-iron-east-03.cleardb.net', 'user', 'pass');
+	$dbc = mysql_connect('us-cdbr-iron-east-03.cleardb.net', 'babcdfac2ed8d3', 'fc9defaa');
 	if(!$dbc) {
 		die('Not Connected : ' . mysql_error()); // die will kill the script
 	}
